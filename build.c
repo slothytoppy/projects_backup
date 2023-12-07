d0c35009775e388f3192dac505bd0191c151d490
slothytoppy <139662903+slothytoppy@users.noreply.github.com>
2023-12-07 13:09:32 -0600

    Update build.c
    added changes to build.c like getting file-extension and proper execution of commands using exec(char* args[])

--- build.c
+++ build.c
@@ -1,35 +1,87 @@
 #include <stdio.h>
-// #include <stdlib.h>
-// #include <fcntl.h>
-#include <unistd.h>
+#include <fcntl.h>
 #include <string.h>
-#ifdef stdlib.h
+#include <unistd.h>
+#include <dirent.h>
+#include <string.h>
+
+#include <sys/wait.h>
+
+#ifdef stdlib_h
 #undef exit
 #endif
 #define exit(code) _exit(code);
 #define print(x) printf("%s", x)
+
+char* fext(char* filename) {
+    char* dot = strrchr(filename, '.');
+    int sz=strlen(filename)-strlen(dot);
+    char* ext=malloc(sz);
+    memcpy(ext, filename, sz);
+    // ext[filename-dot];
+    return ext;
+}
+
+int config_open(void){
+
+}
+
+int exec(char* args[]){
+    // printf("args:%s\n", *args);
+    pid_t id=fork();
+    int child_status;
+    if(id==0){
+    execv(args[0], args);
+    } 
+    if(id<0){
+        printf("forking failed");
+    return -1;
+    }
+    if(id>0){
+    wait(&child_status);
+    // waitpid(-1, NULL, 0);
+    }
+    return 0;
+}
+
 int main(int argc, char* argv[]){
-    if(argv[1]==NULL || argv[2]==NULL){
+    // [TODO] make config resetable, make config deletable, make /usr/bin an optional thing so you can add your own path
+    if(argc<=1){
         printf("not enough args\n");
         exit(1);
     }
-    FILE *fd=fopen("config", "r");
-      if(fd!=NULL && strcmp(argv[1], "-b")==0){
+    const char* file_name="build_config_v1";
+    FILE *fd=fopen(file_name, "r");
+      if(fd!=NULL && argv[1]!=NULL && strcmp(argv[1], "-b")==0){
         char buff[255];
         fscanf(fd, "%s", buff);
-        char* args[]={buff, argv[2], "-o", argv[3], NULL};
-        printf("arg:{%s} arg1:{%s}\n", argv[2], argv[3]);
-        printf("buff:{%s}\n", args[0]);
-        fork();
-        execv(args[0], args);
+        char* args[]={buff, argv[2], "-o", argv[4], NULL};
+        char* arg[]={argv[3], NULL};
+        if((exec(args))==-1){
+            printf("error:");
+        }
+        if((exec(arg))==-1){
+        printf("error:");
+        }
+    } 
+    if(argv[1]!=NULL && strcmp(argv[1], "-c")==0){
+        if(argv[2]==NULL){
+            fprintf(stderr, "must have 2 arguments\n");
+            exit(1);
+        }
+        char* args[]={argv[2], "-o", argv[3], argv[4], NULL};
+        printf("{%s}\n", *args);
+        if((exec(args))==-1){
+            printf("error");
+        }
     }
     if(fd==NULL){
-    fd=fopen("config", "w+");
+    fd=fopen(file_name, "w+");
         char buff[255];
         printf("enter in a compiler:");
         fscanf(stdin, "%s", buff);
-        fprintf(fd, "%s", "/usr/bin/");
-        fprintf(fd, "%s", buff, sizeof(buff));
+        // fprintf(fd, "%s", "/usr/bin/");
+        fprintf(fd, "%s", buff);
     fclose(fd);
     }
 }
