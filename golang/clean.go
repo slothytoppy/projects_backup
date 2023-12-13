ff41cf4098f8a6846185b574cc9ecbde85e3ed64
slothytoppy <139662903+slothytoppy@users.noreply.github.com>
2023-12-13 09:26:44 -0600

    Update clean.go

--- golang/clean.go
+++ golang/clean.go
@@ -1,20 +1,26 @@
 package main
-
 import(
 	"os"
-	"fmt"
+  "fmt"
 	"path"
 	"strings"
 )
+// [TODO]: gets directory path; checks if file(s)/source file(s) in path exist, then you get the basename(file without extension), then remove the file(s) if it exists; 
+// then you would either import this file or run it on a config.go file; end goal is to have this be scriptable, may end up being just function calls
+
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
 
 func stat(file *os.File) (int64){
 	fi, err:=file.Stat(); if err!=nil{
-		fmt.Println(err)
-		os.Exit(1)
+	fmt.Println(err)
+	os.Exit(1)
 	}
 	return fi.Size()
 }
-
 
 func f_exist(file string) bool{
 	if _, err:=os.Stat(file); err!=nil{
@@ -34,35 +40,51 @@ func f_list(list ...string){
 			fmt.Printf("{%s}\n", list)
 			fmt.Printf("deleted:{%s}\n", f_ext(list))
 			os.Remove(f_ext(list))
-			} 
-			if(!f_exist(f_ext(list))){
+			} else{
 				fmt.Printf("file {%s} doesnt exist\n", f_ext(list))
 			}
 		}
   }
 }
+
+func args() int{  
+  return len(os.Args)
+}
+
+func dir_remove(dir string){
+  err:=os.RemoveAll(dir); if err!=nil{
+    fmt.Printf("error removing %s", dir)
+  }
+}
+
+func create(file string){
+  os.Open(file)
+}
+
 func main(){
 	args_c:=len(os.Args)
-	if args_c>=2 && os.Args[1]=="term"{
+	if args()==2 && os.Args[1]=="term"{
 		fmt.Println("usage: term \"args...\"")
 	}
-	if args_c>=2 && os.Args[1]=="term"{
+	if args()>=2 && os.Args[1]=="term"{
 	var max=len(os.Args)
 	var i int=2
-	if(i<max){
-		for len(os.Args)>i{
+	if(i<=max){
+		for args_c>i{
 		f_list(os.Args[i])
 		i++
+	  }
 	}
-	fmt.Printf("count:%d\n%T\n", max-2, args_c)
+} 
+  if args()>=2 && os.Args[1]!="term"{
+	f_exist("tes.c")
+  f_list("tes.c", "gend.c", "perms.go")
 	}
-  } 
-  if args_c>=2 && os.Args[1]!="term"{
-	f_list("tes.c", "gend.c", "perms.go")
-	fmt.Printf("%T\n", args_c)
-	}
-  if args_c<=1{
-	fmt.Printf("usage: command args...\n%T\n", args_c)
+  if args()==1{
+	fmt.Printf("usage: command args...\n")
 	os.Exit(1)
   }
+  if args()>=2 && os.Args[1]=="rdir"{
+  dir_remove(os.Args[2])
+}
 }
