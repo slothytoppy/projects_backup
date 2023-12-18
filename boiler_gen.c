d0beb0e9f4b6015f7c2d6367697b4d6a4d2643b7
slothytoppy <139662903+slothytoppy@users.noreply.github.com>
2023-12-17 19:05:27 -0600

    Update boiler_gen.c

--- boiler_gen.c
+++ boiler_gen.c
@@ -5,28 +5,26 @@
 
 #include <sys/stat.h>
 
-
 int main(int argc, char* argv[]){
     if(argc-1>1){
         write(1 ,"too many arguments\n", 19);
         _exit(1);
     }
-    int fd;
-    fd=open("gend.c", O_RDWR | O_CREAT, 0644);
-    // write(1, "printf(\"hello world\"\n)", 23);
-    write(fd, "#include <stdio.h>\n", 19);
-    write(fd, "#include <fcntl.h>\n", 19);
-    write(fd, "#include <string.h>\n", 20);
-    write(fd, "#include <unistd.h>\n", 20);
-    write(fd, "#include <dirent.h>\n\n", 20);
-    write(fd, "\nint main(int argc, char* argv[]){\n", 35);
-    write(fd, "printf(\"hello world\\n\");\n}", 26);
+    FILE *fd=fopen("gend.c", "w+");
+    fprintf(fd, "#include <stdio.h>\n");
+    fprintf(fd, "#include <string.h>\n");
+    fprintf(fd, "#include <unistd.h>\n");
+    fprintf(fd, "#include <dirent.h>\n");
+    fprintf(fd, "\n");
+    fprintf(fd, "#include <sys/stat.h>\n");
+    fprintf(fd, "#include <fcntl.h>\n");
+    fprintf(fd, "/* written by boiler_gend.c */\n");
+    fprintf(fd, "\nint main(int argc, char* argv[]){\n");
+    fprintf(fd, "printf(\"hello world\\n\");\n}");
+    fclose(fd); 
+    int fp=open("gend.c", O_RDWR, 0644);
     struct stat ft;
-    stat("gend.c", &ft);
-    int size = ft.st_size;
-    char buf[sizeof(ft.st_size)];
-    sprintf(buf,"%d", size);
-    write(1, buf, strlen(buf));
-    write(1, "\n", 1);
-    // write(1, buf, sizeof(buff));
+    fstat(fp, &ft);
+    printf("size:%ld\n", ft.st_size);
+    close(fp);
 }
